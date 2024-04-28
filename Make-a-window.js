288d54f84180990bfe50e875269009db0e02cdc6
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-27 17:51:13 -0700

    Update Make-a-window.js

--- Make-a-window.js
+++ Make-a-window.js
@@ -32,3 +32,5 @@ function makeAWindow(num) {
   let centre = `|${'-'.repeat(num)}+${'-'.repeat(num)}|\n`
   return '-'.repeat(num * 2 + 3) + '\n' + side + centre + side + '-'.repeat(num * 2 + 3) 
 }
+
+// or
